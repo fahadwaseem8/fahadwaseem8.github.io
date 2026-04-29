f8cace6e3d6e407aef9ba6ff40b6406468b30da1
Fahad Waseem <fahadwaseem8@gmail.com>
2026-04-29 21:29:21 +0500

    added github homepage

---
+++ info.js
@@ -0,0 +1,8 @@
+const INFO = {
+  username: "fahadwaseem8",
+  avatar: "https://avatars.githubusercontent.com/u/60786655",
+  github: "https://github.com/fahadwaseem8",
+  telegram: "https://t.me/fahadw9",
+  twitter: "https://x.com/fahadwaseem80",
+  email: "fahadwaseem8@gmail.com",
+};
