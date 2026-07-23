ac3aa718471a551f5b1742c9610d7e8094037c52
Fahad Waseem <60786655+fahadwaseem8@users.noreply.github.com>
2026-07-23 18:58:20 +0500

    refactor: replace Telegram and Twitter with LinkedIn and CV in social links

--- info.js
+++ info.js
@@ -2,7 +2,7 @@ const INFO = {
   username: "fahadwaseem8",
   avatar: "https://avatars.githubusercontent.com/u/60786655",
   github: "https://github.com/fahadwaseem8",
-  telegram: "https://t.me/fahadw9",
-  twitter: "https://x.com/fahadwaseem80",
   email: "fahadwaseem8@gmail.com",
+  linkedin: "https://www.linkedin.com/in/fahadwaseem8/",
+  cv: "https://docs.google.com/document/d/1TxQtOent4wG9xT2mdsi42P5shQb--wtYDp-P85awmmQ/view",
 };
